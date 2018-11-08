0f830daf9e18a4711c8808e69911db298bd9477d
kewinczi <213432@student.pwr.edu.pl>
2018-11-08 11:51:59 +0100

    add colors to pokemon types

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { colors } from '../constants';
 
 class Pokemon extends React.Component {
     render() {
         const { pokemon } = this.props
         return (
             <div className="col-lg-3 col-md-4 col-sm-6 m-0 p-1" style={{width: '18rem'}} key={pokemon.id}>
-                    <div className="card">
+                    <div className="card h-100 d-inline-block w-100 p-3">
                         <div className="card-body">
                         <img className="card-img-top" src={pokemon.img} alt={pokemon.name}/>
                             <h6>{`#${pokemon.num} ${pokemon.name}`}</h6>
-                            <p>{pokemon.type.map((element, index) => <span key={`${pokemon.id}${element}`} className="badge badge-primary m-1">{element}</span>)}</p>
+                            <p>{pokemon.type.map((element, index) => <span style={{'background-color':`${colors[element]}`}}key={`${pokemon.id}${element}`} className="badge badge-primary m-1">{element}</span>)}</p>
                         </div>  
                     </div>
             </div>
---
+++ src/constants.js
@@ -0,0 +1,15 @@
+export const colors = {
+    Grass: 'green',
+    Poison: 'purple',
+    Fire: 'red',
+    Flying: 'blue',
+    Bug: 'grey',
+    Ground: 'brown',
+    Normal: 'lightgray',
+    Dragon: 'orange',
+    Psychic: 'black',
+    Fighting: 'yellow',
+    Electric: 'violet',
+    Rock: 'darkgray',
+    Ice: 'lightblue'
+}
